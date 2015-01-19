2dd8a6c4a3c14acd4af0c978f5c43d66f68dbc9e
Henrik Vester <henrik645@gmail.com>
2015-01-19 08:12:26 +0100

    Rewritten previous code to object oriented code

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import sys
+import copy
+import random
+
 class Room:
     def __init__(self, configuration={}):
-        self.doors = configuration
+        self.doors = copy.deepcopy(configuration)
+
+class Wilderness(Room):
+    def __init__(self, name, description, configuration, monster_percent):
+        super().__init__(configuration)
+        self.name = name
+        self.description = description
+        self.monster_percent = monster_percent
 
 class Merchant:
     def __init__(self, markup=1.2, markdown=0.8):
@@ -43,6 +54,9 @@ class Town(Room):
         self.name = name
         self.description = description
 
+#Types of items are: Item (Normal item), Consumable (Removed after use) and Armour (Normal armour. Defense level is stored at defense) If any item restores HP, the
+#maximum amount of HP restored is stored at hp_restore
+
 class Item:
     def __init__(self, name, description, value):
         self.name = name
@@ -65,16 +79,66 @@ class Weapon(Item):
         self.damage = damage
         super().__init__(name, description, value)
 
+class Consumable(Item):
+    def __init__(self, name, description, value, hp_restore=0):
+        self.hp_restore = hp_restore
+        super().__init__(name, description, value)
+
+class Armour(Item):
+    def __init__(self, name, description, value, defense=0, level_required=1):
+        self.defense = defense
+        self.level_required = level_required
+        super().__init__(name, description, value)
+
 class Player:
-    def __init__(self, room, hp=100, inventory=[], armour=[]):
+    def __init__(self, room, hp=100, max_hp=100, defense=0, money=100, inventory=[], armour=[]):
         self.room = room
         self.hp = hp
+        self.max_hp = max_hp
         self.inventory = inventory
         self.armour = armour
+        self.defense = defense
+        self.money = money
     
-    def go_to(self, room):
+    def go_to_room(self, room):
         self.room = room
-        
+    
+    def view_inventory(self, use_menu=False):
+        if len(self.inventory) > 0:
+            items_formatted = []
+            
+            for item in self.inventory:
+                items_formatted.append(item.name)
+            
+            print("  - Inventory -  ")
+            
+            count = 0
+            if use_menu:
+                for item in items_formatted:
+                    print(" " + str(count) + " " + item)
+                    count += 1
+            else:
+                for item in items_formatted:
+                    print(" * " + item)
+    
+    def view_equipment(self):
+        if len(self.armour) > 0:
+            items_formatted = []
+            
+            for item in self.armour:
+                items_formatted.append(item.name)
+            
+            print("  - Equipment -  ")
+            
+            for item in items_formatted:
+                print(" * " + item)
+      
+class Monster:
+    def __init__(self, name, hp, hp_per_hit):
+        self.name = name
+        self.hp = hp
+        self.hp_per_hit = hp_per_hit
+    
 def view_selling_offers(merchant):
     selling_offers = merchant.merchant.get_selling_offers()
 
@@ -86,50 +150,150 @@ def view_selling_offers(merchant):
     for i in range(len(selling_offers_formatted)):
         print(str(i + 1) + " - " + selling_offers_formatted[i][0] + " - " + str(selling_offers_formatted[i][1]) + "G")
 
- 
-martins_way = []
-town_square = Room()
-martins_way.append(Room())
-martins_way.append(Room())
-tailor = Room()
-blacksmith = Room()
+monster_percent_north_gate = [{'name': 'Wolf', 'level': 1, 'percent': 0.3}, {'name': 'Zombie' ,'level': 2, 'percent': 0.5}]
+
+class Loot_table():
+    def __init__(self, list):
+        self.list = list
+    def choose_from_list(self):
+        random.seed()
+        percent_absolute = []
+        percent_cumulative = []
+        cumulative = 0
+        chosen = None
+        for percent in self.list:
+            percent_absolute.append(percent['percent'])
+            percent_cumulative.append(percent['percent'] + cumulative)
+            cumulative += percent['percent']
+            
+        default = 1 - cumulative
         
-town_square = Town("Town Square", "A stone brick fountain is located in the middle. You see farmers selling their goods.", {'north': martins_way[0]})
-martins_way[0] = Town("Martin's Way", "A wide stone brick road", {'north':martins_way[1], 'south':town_square})
-martins_way[1] = Town("Martin's Way", "A wide stone brick road", {'south':martins_way[0], 'west':tailor, 'east':blacksmith})
-tailor = Town("Tailor", "A heavy-set man is standing behind the oak counter. You see sewing supplies in the back room.", {'east': martins_way[1]})
-blacksmith = Town("Blacksmith", "A little stone building. You feel the heat from the forge.", {'west':martins_way[1]})
+        random_num = random.random()
+        for i, percent in enumerate(percent_cumulative):
+            if percent > random_num:
+                chosen = self.list[i]
+                break
+        
+        if chosen is None:
+            chosen = {}
+        
+        return(chosen)
+    
+directions = {'n': 'north', 'e': 'east', 's': 'south', 'w': 'west'}
+
+town_square = Town("Town Square", "A stone brick fountain is located in the middle. You see farmers selling their goods.")
+tailor = Town("Tailor", "You see sewing supplies in the back.")
+martins_way = [Town("Martin's Way 1", "A wide stone brick road."), Town("Martin's Way 2", "A wide stone brick road.")]
+blacksmith = Town("Blacksmith", "A little stone building. You feel the heat from the forge as soon as you step in.")
+north_gate = Wilderness("North Gate", "A high valved gate. Two guards are patrolling everyone coming in.", {}, monster_percent_north_gate)
+
+tailor.doors['east'] = martins_way[1]
+town_square.doors['north'] = martins_way[0]
+town_square.doors = {'north': martins_way[0]}
+martins_way[0].doors['north'] = martins_way[1]
+martins_way[0].doors['south'] = town_square
+martins_way[1].doors['east'] = blacksmith
+martins_way[1].doors['south'] = martins_way[0]
+martins_way[1].doors['west'] = tailor
+martins_way[1].doors['north'] = north_gate
+north_gate.doors['south'] = martins_way[1]
+blacksmith.doors['west'] = martins_way[1]
 
 tailor.merchant = Merchant(1.0, 0.8)
 blacksmith.merchant = Merchant(1.0, 0.8)
 
-leather_jacket = Item("Leather Jacket", "A large leather jacket. The quality isn't that good.", 5)
-leather_pants = Item("Leather Pants", "A pair of leather pants. The quality isn't that good.", 5)
+leather_jacket = Armour("Leather Jacket", "A large leather jacket. The quality isn't that good.", 5, 10)
+leather_pants = Armour("Leather Pants", "A pair of leather pants. The quality isn't that good.", 5, 10)
 iron_pickaxe = Weapon("Iron Pickaxe", "A simple iron pickaxe. Useful for mining", 10, 10)
 iron_sword = Weapon("Iron Sword", "A simple iron sword. Useful for slaying enemies", 15, 20)
 iron_axe = Weapon("Iron Axe", "A simple iron axe. Useful for cutting down trees", 10, 15)
+apple = Consumable("Apple", "A tasty apple", 3, 10)
 
 tailor.merchant.add_item(leather_jacket)
 tailor.merchant.add_item(leather_pants)
 blacksmith.merchant.add_item(iron_pickaxe)
 blacksmith.merchant.add_item(iron_sword)
 blacksmith.merchant.add_item(iron_axe)
+tailor.merchant.add_item(apple)
 
-player = Player(town_square)
+player = Player(town_square) #Starting room is always town square.
 
-player.go_to(town_square) #Starting room is always town square.
+refresh = True
+player.armour.append(iron_sword)
 
 while True:
-    print(player.room.name)
-    print(player.room)
-    for i in range(30):
+    if refresh:
+        for i in range(30):
+            print("")
+        refresh = False
+        print(player.room.name)
+        print(player.room.description)
+        if hasattr(player.room, 'merchant'):
+            print("")
+            print("A merchant is present.")
+            view_selling_offers(player.room)
+            print("Enter number to buy.")
         print("")
-    print(player.room.name)
-    print(player.room.description)
-    print(player.room.doors)
-    print("Exits:")
-    for exit in player.room.doors:
-        print(exit.title() + " (" + player.room.name + ")")
+        print("Exits:")
+        for exit in player.room.doors:
+            print(exit.title() + " (" + player.room.doors[exit].name + ")")
     walk_to = input(">")
-    print(player.room.doors[walk_to])
-    player.go_to(player.room.doors[walk_to])
+    walk_to = walk_to.lower()
+    if walk_to == 'g' or walk_to == 'gold':
+        print("Gold: " + str(player.money))
+        print("HP: " + str(player.hp))
+        print("Defense: " + str(player.defense))
+    elif walk_to == 'i' or walk_to == 'inventory':
+        player.view_inventory()
+        print("")
+        player.view_equipment()
+    elif walk_to == 'u' or walk_to == 'use':
+        player.view_inventory(True)
+        print("Enter number to use or equip.")
+        use = input(">")
+        if use.isdigit():
+            use = int(use)
+            if use >= 0 and use <= len(player.inventory):
+                if isinstance(player.inventory[use], Consumable):
+                    if player.hp + player.inventory[use].hp_restore > player.max_hp:
+                        player.hp = player.max_hp
+                        print("You were restored to " + str(player.max_hp) + " HP.")
+                    else:
+                        player.hp += player.inventory[use].hp_restore
+                        print("You were restored to " + str(player.hp + player.inventory[use].hp_restore) + " HP.")
+                    player.inventory.pop(use)
+                elif isinstance(player.inventory[use], Armour):
+                    player.armour.append(player.inventory[use]) #Appends value to equipment.
+                    item = player.inventory.pop(use) #Removes value from inventory.
+                    player.defense += item.defense
+                    print("Equipped item.")
+                    print("Your Defense level is " + str(player.defense))
+                else:
+                    print("This item can not be equipped.")
+               
+    else:
+        for dir_abbr, direction in directions.items():
+            if direction in player.room.doors:
+                if walk_to == dir_abbr:
+                    player.go_to_room(player.room.doors[direction])
+                    refresh = True
+                    break
+                elif walk_to.lower() == direction:
+                    player.go_to_room(player.room.doors[direction])
+                    refresh = True
+                    break
+        try:  #Tries if walk_to (player input) is a number, meaning that the player have chosen to buy an item.
+            walk_to = int(walk_to)
+            selling_offers = player.room.merchant.get_selling_offers()
+            walk_to -= 1
+            if walk_to >= 0 and walk_to <= len(selling_offers):
+                if player.money < selling_offers[walk_to][1]:
+                    print("You are not in possession of enough gold.")
+                else:
+                    print("You have bought: " + selling_offers[walk_to][0].name)
+                    player.money -= selling_offers[walk_to][1]
+                    player.inventory.append(selling_offers[walk_to][0])
+            else:
+                print("You cannot find this item in this store.")
+        except:
+            pass
